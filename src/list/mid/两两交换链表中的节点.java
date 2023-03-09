fc6011cdf16317da78237e73079d5b5b2f3ca7b7
changjunhuang <421536376@qq.com>
2023-03-09 23:37:14 +0800

    练习

---
+++ src/list/mid/两两交换链表中的节点.java
@@ -0,0 +1,40 @@
+package list.mid;
+
+import pub.ListNode;
+
+/**
+ * 24. 两两交换链表中的节点
+ */
+public class 两两交换链表中的节点 {
+    public static void main(String[] args) {
+
+    }
+
+    //  递归
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = head.next;
+        head.next = swapPairs(newHead.next);
+        newHead.next = head;
+        return newHead;
+    }
+
+    //  迭代
+    public ListNode swapPairs2(ListNode head) {
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode temp = dummyHead;
+        while (temp.next != null && temp.next.next != null) {
+            ListNode node1 = temp.next;
+            ListNode node2 = temp.next.next;
+            temp.next = node2;
+            node1.next = node2.next;
+            node2.next = node1;
+            temp = node1;
+        }
+        return dummyHead.next;
+    }
+
+}

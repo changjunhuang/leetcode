2f4b2a7854833540951fe00c6dad60ff820cc46f
huangchangjun <421536376@qq.com>
2023-05-25 11:22:00 +0800

    练习

---
+++ src/array/mid/旋转矩阵.java
@@ -0,0 +1,59 @@
+package array.mid;
+
+/**
+ * 面试题 01.07. 旋转矩阵
+ *
+ * @author huangchangjun
+ * @date 2023/5/24
+ */
+public class 旋转矩阵 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 使用辅助数组，空间复杂度为O(n)
+     *
+     * @param matrix
+     */
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+        int[][] matrix_new = new int[n][n];
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                matrix_new[j][n - i - 1] = matrix[i][j];
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                matrix[i][j] = matrix_new[i][j];
+            }
+        }
+    }
+
+    /**
+     * 反转两次,先转行，再转列，空间复杂度O(1)
+     *
+     * @param matrix
+     */
+    public static void rotate2(int[][] matrix) {
+        int n = matrix.length;
+        // 水平翻转
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - i - 1][j];
+                matrix[n - i - 1][j] = temp;
+            }
+        }
+        // 主对角线翻转
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+}
---
+++ src/str/mid/反转每对括号间的子串.java
@@ -0,0 +1,51 @@
+package str.mid;
+
+import java.util.Stack;
+
+/**
+ * 1190. 反转每对括号间的子串
+ *
+ * @author huangchangjun
+ * @date 2023/5/24
+ */
+public class 反转每对括号间的子串 {
+
+    public static void main(String[] args) {
+        String str = "(u(love)i)";
+
+        System.out.println(reverseParentheses(str));
+    }
+
+
+    //  "(u(love)i)"
+    public static String reverseParentheses(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                StringBuilder sb = new StringBuilder();
+                while (stack.peek() != '(') {
+                    sb.append(stack.pop());
+                }
+                //  弹出 (
+                stack.pop();
+                //  重新塞入栈中
+                reverseStr(sb, stack);
+            } else {
+                stack.add(s.charAt(i));
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        while (!stack.empty()) {
+            result.append(stack.pop());
+        }
+        return result.reverse().toString();
+    }
+
+    public static void reverseStr(StringBuilder stringBuilder, Stack<Character> stack) {
+        for (int i = 0; i < stringBuilder.length(); i++) {
+            stack.add(stringBuilder.charAt(i));
+        }
+    }
+}

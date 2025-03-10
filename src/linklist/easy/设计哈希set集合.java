c1e3fc7938f3ac2c06411517e3c319de658264de
huangchangjun <421536376@qq.com>
2025-03-10 11:51:23 +0800

    hot100  package rename

---
+++ src/linklist/easy/设计哈希set集合.java
@@ -0,0 +1,29 @@
+package linklist.easy;
+
+import pub.ListNode;
+
+import java.util.ArrayList;
+
+/**
+ * 705. 设计哈希set集合
+ */
+public class 设计哈希set集合 {
+
+    private ArrayList<ListNode> arrayList;
+
+    public 设计哈希set集合() {
+        arrayList = new ArrayList<>(16);
+    }
+
+    public void add(int key) {
+
+    }
+
+    public void remove(int key) {
+
+    }
+
+    public boolean contains(int key) {
+        return true;
+    }
+}

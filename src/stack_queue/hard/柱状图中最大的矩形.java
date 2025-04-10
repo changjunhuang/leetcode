5e1e6f95e3e69ba35f92cf85addd2692f004fcb5
huangchangjun <421536376@qq.com>
2025-04-10 09:56:34 +0800

    hot100

--- src/stack_queue/hard/柱状图中最大的矩形.java
+++ src/stack_queue/hard/柱状图中最大的矩形.java
@@ -1,12 +1,36 @@
 package stack_queue.hard;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
 /**
  * 84. 柱状图中最大的矩形
+ *
  * @author huangchangjun
  * @date 2025-04-01
  */
 public class 柱状图中最大的矩形 {
     public int largestRectangleArea(int[] heights) {
-        return 0;
+        int n = heights.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Arrays.fill(right, n);
+
+        Deque<Integer> monoStack = new ArrayDeque<Integer>();
+        for (int i = 0; i < n; ++i) {
+            while (!monoStack.isEmpty() && heights[monoStack.peek()] >= heights[i]) {
+                right[monoStack.peek()] = i;
+                monoStack.pop();
+            }
+            left[i] = (monoStack.isEmpty() ? -1 : monoStack.peek());
+            monoStack.push(i);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            ans = Math.max(ans, (right[i] - left[i] - 1) * heights[i]);
+        }
+        return ans;
     }
 }
